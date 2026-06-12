a67ae45b94e53d2b15cca75a2a59bb459ca21ed8
Anshu Goyal <160588740+AnshuGoyal-0@users.noreply.github.com>
2026-06-12 19:19:23 +0000

    updated

--- Calculator (CLI + GUI)/Calculator_cli.py
+++ Calculator (CLI + GUI)/Calculator_cli.py
@@ -1,38 +1,49 @@
 #import layer
 #functions layer
 def add(a,b):
-    print(a+b)
+    return (a+b)
 
 def sub(a,b):
-    print(a-b)
+    return (a-b)
 
 def mul(a,b):
-    print(a*b)
+    return (a*b)
 
 def div(a,b):
-    print(a/b)
+    return (a/b)
 
 #Coding layer
-
-
-
-while 1:
+choice = ""
+while True:
+    choice = input("enter the name of operation you want to perform:- \n1.add\n2.sub\n3.mul\n4.div\n5.stop\n\n").lower()
     if(choice=="stop"):
         break
     else:
-        choice = input("enter her digit you want to perform:- \n1.add\n2.sub\n3.mul\n4.div\n5.stop\n")
-        num1 = input("enter you first digit : ")
-        num2 = input("enter you second digit : ")
-        switch(choice):
-            case "add":
-                add(num1,num2)
-            case "sub":
-                sub(num1,num2)
-            case "mul":
-                mul(num1,num2)
-            case "div":
-                div(num1,num2)
-            default:
-                continue
+        num1 = int(input("enter you first digit : "))
+        num2 = int(input("enter you second digit : "))
+
+        if(choice in ["add","sum","plus","+"]):
+            print(add(num1,num2))
+            
+        elif(choice in ["minus","sub","subtract","-"]):
+            print(sub(num1,num2))
+            
+        elif(choice in ["into","mul","guda","*"]):
+            print(mul(num1,num2))
+            
+        elif(choice in ["divison","div","bhag","/"]):
+            if(num2=="0"):
+                print("division error")
+            else:
+                print(div(num1,num2))
+            
+        else:
+            print("operation not found try again")
+            choice = ""
+            continue
+        choice = ""
+        continue
+            
+            
 
 
