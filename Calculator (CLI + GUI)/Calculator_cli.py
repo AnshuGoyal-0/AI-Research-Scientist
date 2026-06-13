394157573a8ffe12af32b817a3b477d6e30d8286
AnshuGoel <codemasteranshu@gmail.com>
2026-06-13 16:43:20 +0530

    y

--- Calculator (CLI + GUI)/Calculator_cli.py
+++ Calculator (CLI + GUI)/Calculator_cli.py
@@ -13,9 +13,9 @@ def div(a,b):
     return (a/b)
 
 #Coding layer
-choice = ""l
+choice = ""
 while True:
-    choice = input("enter the name of operation you want to perform:- \n1.add\n2.sub\n3.mul\n4.div\n5.stop\n\n").lower()
+    choice = input("enter the name of operation you want to perform:- \n1.add\n2.sub\n3.mul\n4.div\n5.stop\n\nEnter : \n\n").lower()
     if(choice=="stop"):
         break
     else:
